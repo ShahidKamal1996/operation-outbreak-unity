503870e9a2d84a6a6ae4954ca8e909ce5d489999
Arena Agent <arena-agent@arena.ai>
2026-08-11 19:43:23 +0000

    Milestone 1I.1: remove obsolete Unity API warning

--- Assets/_OperationOutbreak/Scripts/UI/GameOverController.cs
+++ Assets/_OperationOutbreak/Scripts/UI/GameOverController.cs
@@ -15,7 +15,7 @@ namespace OperationOutbreak.UI
   void OnDisable(){ if(playerHealth!=null) playerHealth.Died-=Show; }
   void Build(){
    var canvas=new GameObject("GameOverCanvas",typeof(RectTransform),typeof(Canvas),typeof(CanvasScaler),typeof(GraphicRaycaster)); canvas.transform.SetParent(transform,false); var c=canvas.GetComponent<Canvas>();c.renderMode=RenderMode.ScreenSpaceOverlay;c.sortingOrder=30; var sc=canvas.GetComponent<CanvasScaler>();sc.uiScaleMode=CanvasScaler.ScaleMode.ScaleWithScreenSize;sc.referenceResolution=new Vector2(1080,1920);
-   if(FindFirstObjectByType<EventSystem>()==null){var es=new GameObject("EventSystem",typeof(EventSystem),typeof(InputSystemUIInputModule));es.transform.SetParent(transform,false);}
+   if(FindAnyObjectByType<EventSystem>()==null){var es=new GameObject("EventSystem",typeof(EventSystem),typeof(InputSystemUIInputModule));es.transform.SetParent(transform,false);}
    panel=new GameObject("GameOverPanel",typeof(RectTransform),typeof(Image));panel.transform.SetParent(canvas.transform,false);var r=(RectTransform)panel.transform;r.anchorMin=Vector2.zero;r.anchorMax=Vector2.one;r.offsetMin=r.offsetMax=Vector2.zero;panel.GetComponent<Image>().color=new Color(0,0,0,.72f);
    Text("GAME OVER",panel.transform,new Vector2(0,130),64);
    var b=new GameObject("RestartButton",typeof(RectTransform),typeof(Image),typeof(Button));b.transform.SetParent(panel.transform,false);var br=(RectTransform)b.transform;br.anchorMin=br.anchorMax=new Vector2(.5f,.5f);br.sizeDelta=new Vector2(340,110);br.anchoredPosition=new Vector2(0,-40);b.GetComponent<Image>().color=new Color(.2f,.75f,.32f,1);b.GetComponent<Button>().onClick.AddListener(Restart);Text("RESTART",b.transform,Vector2.zero,38);
